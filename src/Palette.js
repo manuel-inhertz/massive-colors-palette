213ddc89f8dd3344227f5ad459610ee7553f17a3
Manuel Barbiero <manuel@bizen.it>
2020-07-07 12:14:01 +0200

    Style color slider

--- src/Palette.js
+++ src/Palette.js
@@ -5,38 +5,40 @@ import "rc-slider/assets/index.css";
 import "./Palette.css";
 
 class Palette extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      level: 500
-    };
-    this.changeLevel = this.changeLevel.bind(this);
-  }
-  changeLevel(level) {
-    this.setState({level});
-  }
-  render() {
-    const {colors} = this.props.palette;
-    const {level} = this.state;
-    const colorBoxes = colors[level].map((color, i) => (
-      <ColorBox background={color.hex} name={color.name} key={`palette-${i}`} />
-    ))
-    return (
-      <div className='Palette'>
-        <Slider 
-          defaultValue={this.state.level} 
-          min={100} 
-          max={900} 
-          step={100} 
-          onAfterChange={this.changeLevel}
-        />
-        {/* Navbar goes here */}
-        <div className='Palette-colors'>
-          {colorBoxes}
-        </div>
-        {/* footer eventually */}
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            level: 500
+        };
+        this.changeLevel = this.changeLevel.bind(this);
+    }
+    changeLevel(level) {
+        this.setState({ level });
+    }
+    render() {
+        const { colors } = this.props.palette;
+        const { level } = this.state;
+        const colorBoxes = colors[level].map((color, i) => (
+            <ColorBox background={color.hex} name={color.name} key={`palette-${i}`} />
+        ))
+        return (
+            <div className='Palette'>
+              <div className="slider">
+                <Slider 
+                  defaultValue={this.state.level} 
+                  min={100} 
+                  max={900} 
+                  step={100} 
+                  onAfterChange={this.changeLevel}
+                />
+              </div>
+              {/* Navbar goes here */}
+              <div className='Palette-colors'>
+                {colorBoxes}
+              </div>
+              {/* footer eventually */}
+            </div>
+        );
+    }
 }
 export default Palette;
